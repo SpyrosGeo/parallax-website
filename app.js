651bb0a1af4273b6c59cdfcf669caaf816305864
SpyrosGeo <xarodasimba@gmail.com>
2021-04-16 18:49:56 +0300

    tweaked the animation more

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let timeline = new TimelineMax();
 // timeline.to(".bg1",3,{y:-300})
 timeline
     .fromTo(".bg1",{y:-50},{y:0,duration:3})
-    .to(".content",3,{top:"0%"})
+    .to(".content",4,{top:"0%"})
 
 
 let scene = new ScrollMagic.Scene({
