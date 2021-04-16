d638b0a0a5bac27d59db4d14c0dbc024452092ec
SpyrosGeo <xarodasimba@gmail.com>
2021-04-16 18:43:38 +0300

    tweaked the timing of the animation a bit

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 // timeline.to(".bg1",3,{y:-300})
-timeline.fromTo(".bg1",{y:-50},{y:0,duration:3}).to(".content",3,{top:"0%"})
+timeline
+    .fromTo(".bg1",{y:-50},{y:0,duration:3})
+    .to(".content",3,{top:"0%"})
 
 
 let scene = new ScrollMagic.Scene({
