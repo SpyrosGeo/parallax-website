67b60b95b8926f215b5ba9d06f6a8c82134d5126
SpyrosGeo <xarodasimba@gmail.com>
2021-04-16 18:54:33 +0300

    final product

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ let timeline = new TimelineMax();
 
 // timeline.to(".bg1",3,{y:-300})
 timeline
-    .fromTo(".bg1",{y:-50},{y:0,duration:3})
+    .fromTo(".bg1",{y:-50},{y:0,duration:6})
     .to(".content",4,{top:"0%"})
+    .fromTo('.content-images',{opacity:0},{opacity:1,duration:1.5})
+    .fromTo('.text',{opacity:0},{opacity:1,duration:1.5})
 
 
 let scene = new ScrollMagic.Scene({
     triggerElement:'section',
-    duration:"100%",
+    duration:"190%",
     triggerHook:0,//defines where do u want to trigger the animation
 })
 .setTween(timeline)
