d4d626b92d7f28b3827a20615b1954b7c201143b
SpyrosGeo <xarodasimba@gmail.com>
2021-04-16 18:39:19 +0300

    fixed a bug with scrolling

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
-timeline.to(".bg1",3,{y:300})
+// timeline.to(".bg1",3,{y:-300})
+timeline.fromTo(".bg1",{y:-50},{y:0,duration:3}).to(".content",3,{top:"0%"},"-=1")
 
 
 let scene = new ScrollMagic.Scene({
@@ -12,5 +13,5 @@ let scene = new ScrollMagic.Scene({
     triggerHook:0,//defines where do u want to trigger the animation
 })
 .setTween(timeline)
-// .setPin("section")
+.setPin("section")
 .addTo(controller)
